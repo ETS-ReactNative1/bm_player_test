abb3b63a494c4bf016a8748a5348619f342e541c
Rihards Neimanis <rihards.neimanis@gmail.com>
2021-02-12 18:22:58 +0200

    Removed unnecessary prop

--- app/components/BitmovinPlayer/BitmovinPlayer.js
+++ app/components/BitmovinPlayer/BitmovinPlayer.js
@@ -43,11 +43,6 @@ class BitmovinPlayer extends React.Component {
     },
   };
 
-  playerSource = {
-    hls:
-      'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
-  };
-
   getVideoContainer() {
     return this.videoContainer.current;
   }
